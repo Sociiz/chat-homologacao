758f64c9751e32aa12fe516ee46b1fb230145a0e
Arthur Vieira <Arthurv720@gmail.com>
2025-10-22 17:08:21 -0300

    (feat) - Adicionado chat dentro da página carregado pelo env.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { useEffect } from 'react';
 import logoSociiz from './assets/logo-sociiz.png'
 import backgroundSociiz from './assets/mulher-olhando-para-o-horizonte.f3df96c9088f21b1.jpg'
 
 function App() {
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://main.d36fuu7zqj3xw9.amplifyapp.com/widget.js";
+    script.async = true;
+    script.setAttribute("clientKey", import.meta.env.VITE_CLIENT_KEY as string);
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
   return (
     <div
       className="relative w-screen h-screen bg-cover bg-center flex items-center justify-center text-white"
@@ -12,7 +25,6 @@ function App() {
         alt="Logo Sociiz"
         className="absolute top-6 left-6 w-40 h-auto"
       />
-
       <h2 className="text-2xl md:text-4xl font-mono text-center drop-shadow-lg">
         JUNTOS, PENSANDO NO FUTURO.
       </h2>
