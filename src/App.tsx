4b23e9a9503dec80861fb2f715c6e60ebb2e2d50
Arthur Vieira <Arthurv720@gmail.com>
2025-10-22 16:47:08 -0300

    (style) - Adicionado background com frase e logo da Sociiz á esquerda

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import logoSociiz from './assets/logo-sociiz.png'
+import backgroundSociiz from './assets/mulher-olhando-para-o-horizonte.f3df96c9088f21b1.jpg'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div
+      className="relative w-screen h-screen bg-cover bg-center flex items-center justify-center text-white"
+      style={{ backgroundImage: `url(${backgroundSociiz})` }}
+    >
+      <img
+        src={logoSociiz}
+        alt="Logo Sociiz"
+        className="absolute top-6 left-6 w-40 h-auto"
+      />
+
+      <h2 className="text-2xl md:text-4xl font-mono text-center drop-shadow-lg">
+        JUNTOS, PENSANDO NO FUTURO.
+      </h2>
+    </div>
   )
 }
 
